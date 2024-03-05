41102fca720f1dfe841cc17a8b3460e47a9f0c22
Karel Puerto Diaz <karelpuerto78@gmail.com>
2024-03-05 14:50:02 -0600

    generate migrations for Rols, Users and Subordinate

---
+++ db/migrations/1709656836536-Subordinate.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Subordinate1709656836536 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE [dbo].[subordinates] (
+              [id] INT PRIMARY KEY IDENTITY(1,1),
+              [boss_id] uniqueidentifier,
+              [subordinate_id] uniqueidentifier,
+              [created_at] datetime2(6) NOT NULL,
+              [updated_at] datetime2(6) NOT NULL,
+              CONSTRAINT FK_subordinate_boss FOREIGN KEY (boss_id) REFERENCES users(id),
+              CONSTRAINT FK_subordinate_id FOREIGN KEY (subordinate_id) REFERENCES users(id)
+          );`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE [dbo].subordinates`);
+  }
+}
